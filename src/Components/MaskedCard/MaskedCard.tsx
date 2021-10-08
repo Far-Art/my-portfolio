a9df12e379f64a19ced2262373d9aa2c37b95020
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-08 17:27:19 +0300

    MaskedCard component added

---
+++ src/Components/MaskedCard/MaskedCard.tsx
@@ -0,0 +1,11 @@
+import "./MaskedCard.css";
+
+export default function MaskedCard(): JSX.Element {
+    return (
+        <div className="MaskedCard">
+            <div className="MaskedCard__Mask">
+                <div className="MaskedCard__Image"></div>
+            </div>
+        </div>
+    );
+}
