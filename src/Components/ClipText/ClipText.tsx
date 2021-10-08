15df4318bee2c2d163b9fc957658b56bf57d4567
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-08 18:29:23 +0300

    Added new props to ClipText components

--- src/Components/ClipText/ClipText.tsx
+++ src/Components/ClipText/ClipText.tsx
@@ -4,9 +4,12 @@ import "./ClipText.css";
 interface ClipTextProps {
     text: string;
     split?: "whitespace";
+    fontSize?: string;
     image?: string;
+    imageFloat?: "left" | "center" | "right" | "top" | "bottom";
     enableShadow?: boolean;
     textShadow?: string;
+    filter?: string;
 }
 
 export default function ClipText(props: ClipTextProps): JSX.Element {
@@ -34,8 +37,11 @@ export default function ClipText(props: ClipTextProps): JSX.Element {
         if (props.image) {
             styleObj.color = "transparent";
         }
+        if (props.fontSize) {
+            styleObj.fontSize = `calc(var(--font-size) + ${props.fontSize})`;
+        }
         setTextStyle(styleObj);
-    }, [props.image]);
+    }, [props.image, props.fontSize]);
 
     /* Text Container Style  */
     useEffect(() => {
@@ -43,8 +49,14 @@ export default function ClipText(props: ClipTextProps): JSX.Element {
         if (props.image) {
             styleObj.backgroundImage = `url(${props.image})`;
         }
+        if (props.imageFloat) {
+            styleObj.backgroundPosition = props.imageFloat;
+        }
+        if (props.filter) {
+            styleObj.filter = props.filter;
+        }
         setTextContainerStyle(styleObj);
-    }, [props.image]);
+    }, [props.image, props.filter]);
 
     /*  Shadow Container Style  */
     useEffect(() => {
@@ -52,16 +64,19 @@ export default function ClipText(props: ClipTextProps): JSX.Element {
         if (props.enableShadow && props.textShadow) {
             styleObj.textShadow = `${props.textShadow}`;
         }
+        if (props.filter) {
+            styleObj.filter = props.filter;
+        }
         setShadowContainerStyle(styleObj);
-    }, [props.enableShadow ,props.textShadow]);
+    }, [props.enableShadow, props.textShadow, props.filter]);
 
     return (
         <div className="ClipText" >
             <div className="ClipTextShadowContainer" style={shadowContainerStyle}>
-                {text.map((str, index) => <h6 key={index} style={textStyle}>{str}</h6>)}
+                {text.map((str, index) => <h1 key={index} style={textStyle}>{str}</h1>)}
             </div>
             <div style={textContainerStyle} className="ClipTextContainer">
-                {text.map((str, index) => <h2 key={index} style={textStyle}>{str}</h2>)}
+                {text.map((str, index) => <h1 key={index} style={textStyle}>{str}</h1>)}
             </div>
         </div>
     );
