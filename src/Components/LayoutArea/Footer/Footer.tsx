f3b48c546b5cb6ffec810151ad21fb607bdf7645
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-12 10:15:29 +0300

    added credits page button

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import "./Footer.css";
 import packageJson from "../../../../package.json";
+import { useHistory } from "react-router-dom";
 
 export default function Footer(): JSX.Element {
+
+    const history = useHistory();
+
     return (
         <div className="Footer">
             <p>{`All rights reserved to © Artur Farmanov`}</p>
+            <button onClick={() => {
+                history.push("/credits");
+            }}>Credits</button>
             <p>app version {packageJson.version}</p>
         </div>
     );
