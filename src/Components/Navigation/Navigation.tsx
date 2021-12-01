fdd2d69c27c9aaf02fd34b051d8227090018675d
Artur Farmanov <arturfarmanov91@gmail.com>
2021-12-01 19:00:29 +0200

    new Navigation component

---
+++ src/Components/Navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import "./Navigation.css";
+
+interface NavigationProps {
+    ref?: React.RefObject<HTMLElement>;
+}
+export default function Navigation(props: NavigationProps): JSX.Element {
+    const navItems = ["home", "projects", "skills", "about"];
+
+    // const myRef = useRef<HTMLDivElement>(null);
+
+    const handleClick = (event: React.MouseEvent | React.TouchEvent) => {
+        if (props.ref?.current) {
+            console.log(props.ref.current)
+            props.ref.current.scrollIntoView();
+        }
+        // window.scrollTo({
+        //     top: 2000,
+        //     behavior: "smooth"
+        // })
+    }
+
+    return (
+        <div className="Navigation">
+            {/* {navItems.map((item, index) => <a href={`#${item}`} key={index}>{item}</a>)} */}
+            {navItems.map((item, index) => <p onClick={(event) => { handleClick(event) }} key={index}>{item}</p>)}
+        </div>
+    );
+}
