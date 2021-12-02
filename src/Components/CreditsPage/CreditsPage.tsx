bdc4f0c9d1fdecefca0aae59fb8ab80ef55bca2b
Artur Farmanov <arturfarmanov91@gmail.com>
2021-12-02 17:33:53 +0200

    updated to router-dom v6

--- src/Components/CreditsPage/CreditsPage.tsx
+++ src/Components/CreditsPage/CreditsPage.tsx
@@ -1,15 +1,15 @@
-import { useHistory } from "react-router";
+import { useNavigate } from "react-router";
 import "./CreditsPage.css";
 
 export default function CreditsPage(): JSX.Element {
-    const history = useHistory();
+    const navigate = useNavigate();
     return (
         <div className="CreditsPage">
             <p> parallax hover cards inspired by <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/andymerskin/pen/XNMWvQ">andymerskin</a></p>
             <p> Clouds background image <a target="_blank" rel="noopener noreferrer" href="https://mocah.org/340048-sunset-clouds-scenery-horizon-art-digital-art.html">mocah.org</a></p>
             <p> Shadows <a target="_blank" rel="noopener noreferrer" href="https://tobiasahlin.com/blog/layered-smooth-box-shadows/">tobiasahlin.com</a></p>
             <button onClick={() => {
-                history.goBack();
+                navigate("/");
             }}>Go back</button>
         </div>
     );
