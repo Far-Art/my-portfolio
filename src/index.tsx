2001c955359727544a55563dc151942fea5835b9
Artur Farmanov <arturfarmanov91@gmail.com>
2021-12-02 17:33:11 +0200

    added provider to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./Styles/GlobalStyles.css";
+import "./Styles/Fonts.css";
 import Layout from "./Components/LayoutArea/Layout/Layout";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/Store";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout />
+      <Provider store={store}>
+        <Layout />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
