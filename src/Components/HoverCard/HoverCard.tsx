36e233cf8e602134eddf2a4c406bbdb85dd5d32b
Artur Farmanov <arturfarmanov91@gmail.com>
2021-12-03 13:48:29 +0200

    removed unnecesary variables, moved some styles to css file, overall code cleanup

--- src/Components/HoverCard/HoverCard.tsx
+++ src/Components/HoverCard/HoverCard.tsx
@@ -19,21 +19,20 @@ export default function HoverCard(props: HoverCardProps): JSX.Element {
 
     const [titleStyle, setTitleStyle] = useState<CSSProperties>({});
 
-    const imageTranslade3d = "translate3d(0, 0, -150px)";
+    const imageTranslade3d = "translate3d(0, 0, -200px)";
 
-    const titleTranslade3d = "translate3d(0, 0, 40px)";
-
-    const transitionOnMouseMove = "transform 70ms linear";
+    const titleTranslade3d = "translate3d(0, 0, 60px)";
 
     const transitionOnLeave = "transform 1s cubic-bezier(.42,.01,.56,1.02)";
 
     const returnToOrigin: CSSProperties = { transform: `rotateX(${0}deg) rotateY(${0}deg)`, transition: transitionOnLeave };
 
-    const maxYDeg = 8; // determines maximum rotation degree for Y
-    const maxXDeg = 8; // determines maximum rotation degree for X
-    const rotationSensivity = 0.02; // determines how fast rotation vill occurr
+    const maxYDeg = 7; // determines maximum rotation degree for Y
+    const maxXDeg = 7; // determines maximum rotation degree for X
+    const rotationSensivity = 0.05; // determines how fast rotation vill occurr
+    const toOriginTimeoutMillis = 1300; // milliseconds before card return to original position
 
-    let returnToOriginTimeout: NodeJS.Timeout;
+    // let returnToOriginTimeout: NodeJS.Timeout;
 
     useEffect(() => {
         if (props.marginBottom) {
@@ -41,7 +40,7 @@ export default function HoverCard(props: HoverCardProps): JSX.Element {
         }
     }, [props.marginBottom]);
 
-    function mouseMoveHandler(event: any) {
+    const mouseMoveHandler = (event: any) => {
         // get target coordinates
         const rect = event.target.getBoundingClientRect();
 
@@ -57,42 +56,34 @@ export default function HoverCard(props: HoverCardProps): JSX.Element {
         let rotationDegY = offsetY > 0 ? Math.min(offsetY * rotationSensivity, maxYDeg) : Math.max(offsetY * rotationSensivity, maxYDeg * -1);
         let rotationDegX = offsetX > 0 ? Math.min(offsetX * rotationSensivity, maxXDeg) : Math.max(offsetX * rotationSensivity, maxXDeg * -1);
 
-
-        // define styles
-        const containerStyle: CSSProperties = { transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg)`, transition: transitionOnMouseMove }
-        const titleStyle: CSSProperties = { transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg) ${titleTranslade3d}`, transition: transitionOnMouseMove };
-        const imageStyle: CSSProperties = { transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg) ${imageTranslade3d}`, transition: transitionOnMouseMove }
-
         //set styles
-        setContainerStyle(containerStyle);
-        setTitleStyle(titleStyle);
-        setImageStyle(imageStyle);
+        setContainerStyle({ transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg)` });
+        setTitleStyle({ transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg) ${titleTranslade3d}` });
+        setImageStyle({ transform: `rotateX(${rotationDegX}deg) rotateY(${rotationDegY}deg) ${imageTranslade3d}` });
     }
+
+    let returnToOriginTimeout = () => setTimeout(() => {
+        setContainerStyle(returnToOrigin);
+        setImageStyle({ transform: returnToOrigin.transform + ` ${imageTranslade3d}`, transition: transitionOnLeave });
+        setTitleStyle({ transform: returnToOrigin.transform + ` ${titleTranslade3d}`, transition: transitionOnLeave });
+    }, toOriginTimeoutMillis);
 
     return (
         <div
             className="HoverCardField"
             style={{ ...fieldStyle }}
-            onMouseEnter={() => clearTimeout(returnToOriginTimeout)}
+            onMouseEnter={() => clearTimeout(returnToOriginTimeout())}
             onTouchMove={(event) => mouseMoveHandler(event)}
             onMouseMove={(event) => mouseMoveHandler(event)}
-            onTouchEnd={() => returnToOriginTimeout = setTimeout(() => {
-                setContainerStyle(returnToOrigin);
-                setImageStyle({ transform: returnToOrigin.transform + ` ${imageTranslade3d}`, transition: transitionOnLeave });
-                setTitleStyle({ transform: returnToOrigin.transform + ` ${titleTranslade3d}`, transition: transitionOnLeave });
-            }, 1300)}
-            onMouseOut={() => returnToOriginTimeout = setTimeout(() => {
-                setContainerStyle(returnToOrigin);
-                setImageStyle({ transform: returnToOrigin.transform + ` ${imageTranslade3d}`, transition: transitionOnLeave });
-                setTitleStyle({ transform: returnToOrigin.transform + ` ${titleTranslade3d}`, transition: transitionOnLeave });
-            }, 1300)}>
+            onTouchEnd={() => returnToOriginTimeout()}
+            onMouseOut={() => returnToOriginTimeout()}>
 
             <div
                 className="HoverCard__container"
                 style={containerStyle}>
                 <div
                     className="HoverCard__image"
-                    style={{ ...imageStyle, background: `url(${props.image})`, backgroundSize: "90%", backgroundPosition: "center", backgroundRepeat: "repeat" }} />
+                    style={{ ...imageStyle, backgroundImage: `url(${props.image})` }} />
                 {props.src && <a href={props.src} rel="noopener noreferrer" target="_blank" className="HoverCardLink"></a>}
             </div>
 
@@ -104,7 +95,6 @@ export default function HoverCard(props: HoverCardProps): JSX.Element {
                     className="HoverCard__apistack"
                 >{props.apiStack && props.apiStack.map((stack, index) => <p key={index}>{stack}</p>)}</div>
             </div>
-
         </div>
     );
 }
