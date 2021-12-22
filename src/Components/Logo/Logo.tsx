d17fe0d441b9b1de9c873606eb94e42b101ce47c
Artur Farmanov <arturfarmanov91@gmail.com>
2021-12-22 09:50:51 +0200

    added alt prop

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import LogoSvg from "../../Assets/Logo/Logo.svg";
 export default function Logo(): JSX.Element {
     return (
         <div className="Logo">
-			<img className="LogoImage" src={LogoSvg}/>
+			<img className="LogoImage" src={LogoSvg} alt="Logo"/>
             <p className="LogoText">artur farmanov</p>
             <p className="LogoTextPortfolio">portfolio</p>
         </div>
